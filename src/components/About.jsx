5bbade623a95d0462a5fb5c06f9897af8856d10f
PostolacheDavid <davidp6plus@gmail.com>
2025-04-09 19:18:04 +0300

    Added profile picture

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,11 @@
+import profile_picture from "../assets/images/profile_picture4.png";
+
 const About = () => {
-  return <section id="about" className="about-section"></section>;
+  return (
+    <section id="about" className="about-section">
+      <img src={profile_picture} alt="Profile Picture" />
+    </section>
+  );
 };
 
 export default About;
