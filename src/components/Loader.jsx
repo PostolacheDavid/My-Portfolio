a1f38b8e0883fad61594b62f7d1812a9455d94c9
PostolacheDavid <davidp6plus@gmail.com>
2025-04-09 12:35:21 +0300

    Finished loaders animations

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,16 +2,23 @@ import { useState, useEffect } from "react";
 
 const Loader = () => {
   const [showName, setShowName] = useState(false);
+  const [moveUp, setMoveUp] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const nameTimer = setTimeout(() => {
       setShowName(true);
-    }, 5000);
-    return () => clearTimeout(timer);
+    }, 2500);
+    const moveUpTimer = setTimeout(() => {
+      setMoveUp(true);
+    }, 4500);
+    return () => {
+      clearTimeout(nameTimer);
+      clearTimeout(moveUpTimer);
+    };
   }, []);
 
   return (
-    <div className="loader-overlay">
+    <div className={`loader-overlay ${moveUp ? "move-up" : ""}`}>
       <div className={`loader-wrapper ${showName ? "slide-left" : ""}`}>
         <div className="loader"></div>
         <p className={`loader-name ${showName ? "slide-out" : ""}`}>
