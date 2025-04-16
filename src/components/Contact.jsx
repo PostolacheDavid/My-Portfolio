42763a7bf08641c299a387edf865beceb5e86511
PostolacheDavid <davidp6plus@gmail.com>
2025-04-16 17:38:31 +0300

    Set up contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+const Contact = () => {
+  return (
+    <section id="Contact" className="contact-section">
+      <div className="contact-wrapper">
+        <article className="contact-info">
+          <h1>Interested in working together?</h1>
+          <p>
+            I’m always open to discussing new projects, creative ideas, or
+            opportunities to be part of your visions. Drop me a message and I’ll
+            get back to you soon.
+          </p>
+        </article>
+        <article className="contact-form"></article>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import About from "./About";
 import Projects from "./Projects";
+import Contact from "./Contact";
 
 const Main = () => {
   return (
     <main>
       <About />
       <Projects />
+      <Contact />
     </main>
   );
 };
