e8ee5fd2784082d309bc34e791cd8af277c784a5
PostolacheDavid <davidp6plus@gmail.com>
2025-04-17 12:07:28 +0300

    added contact-info ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,6 @@
+import { IoMail, IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+
 const Contact = () => {
   return (
     <section id="Contact" className="contact-section">
@@ -9,6 +12,22 @@ const Contact = () => {
             opportunities to be part of your visions. Drop me a message and I’ll
             get back to you soon.
           </p>
+          <h2>Contact Info</h2>
+          <p>
+            <IoMail className="react-icon" />
+            Email address:{" "}
+            <a href="mailto:youremail@example.com">
+              contact@davidpostolache.com
+            </a>
+          </p>
+          <p>
+            <FaLocationDot className="react-icon2" />
+            Location: Bucharest, Romania
+          </p>
+          <div className="socials">
+            <IoLogoGithub className="socials-logo" />
+            <IoLogoLinkedin className="socials-logo" />
+          </div>
         </article>
         <article className="contact-form"></article>
       </div>
